fde27f6e9f2325fda647259a55a19a66c7a48e0e
Nicholas671 <nicholas.a.crabtree@gmail.com>
2025-01-13 08:55:40 -0700

    list of freelancers added

--- Unit2.Shapes.Starter/script.js
+++ Unit2.Shapes.Starter/script.js
@@ -1,4 +1,4 @@
-//List on initial freelancers
+//List on additional freelancers
 const addNames = [
     { name: "Aurra Sing", species: "Paaliduvan", price: "20000" },
     { name: "Cad Bane", species: "Duros", price: "70000" },
@@ -8,4 +8,13 @@ const addNames = [
     { name: "Greedo", species: "Rodian", price: "30000" },
     { name: "Black Krrsantan", species: "Wookie", price: "100000" },
     { name: "Din Djarin", species: "Human", price: "250000" },
+]
+//list of initial freelancers
+const names = [
+    { name: "Boba Fett", species: "Human", price: "500000" },
+    { name: "Bossk", species: "Trandoshan", price: "100000" },
+    { name: "IG-88", species: "Droid", price: "150000" },
+    { name: "Dengar", species: "Human", price: "85000" },
+    { name: "4-LOM", species: "Droid", price: "75000" },
+    { name: "Zuckuss", species: "Gand", price: "75000" },
 ]
